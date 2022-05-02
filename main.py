18f57c74e3776caa61888053e6f3967e0840b9e3
ts00193189 <marero9308@gmail.com>
2022-05-02 15:22:11 +0800

    Add migrations

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from flask_migrate import Migrate
 from todo import create_app
 from todo.models import db, User, Todo
 
 app = create_app('development')
+migrate = Migrate(app, db)
+
 
 @app.shell_context_processor
 def make_shell_context():
